44c2d288846e047c2351c4f8a27064f230eb42eb
ymfact <123young321@naver.com>
2014-04-15 19:09:06 +0900

    불 그래픽이 무서워서 수정함

--- GAssn2/GAssn1/hurdles.cpp
+++ GAssn2/GAssn1/hurdles.cpp
@@ -13,7 +13,7 @@ void JarFire::update()
 	if( ++tick == 60 )
 		tick = 0;
 
-	rotation = sin( (double)tick / 30 * 3.14 ) * 5;
+	rotation = sin( (double)tick / 30 * 3.14 ) * 6;
 
 	int t = tick<30?tick:60-tick;
 	double s = sin(t / 300.0) + 1;
@@ -63,24 +63,12 @@ void Jar::draw()
 	drawVertex2f( +18, -15 );
 	drawEnd();
 }
-
-RingFire::RingFire( GameObject* parent, Vector2& position, int color, double fireScale, int tick, int derivation)
-	: GameObject( parent, position ),
-	color( color ), fireScale( fireScale ), tick( tick ), derivation(derivation)
+RingFireGraphic::RingFireGraphic( GameObject* parent, int color, double fireScale )
+	: GameObject( parent, Vector2(), 0, Vector2( fireScale ) ), color( color )
 {
-}
-void RingFire::update()
-{
-	if( ++tick == 60 )
-		tick = 0;
 
-	rotation = derivation + sin( (double)tick / 30 * 3.14 ) * 15;
-
-	int t = tick<30?tick:60-tick;
-	double s = sin(t / 300.0) + 1;
-	scale = fireScale * Vector2( s );
 }
-void RingFire::draw()
+void RingFireGraphic::draw()
 {
 	setColor( static_cast<COLOR>(color) );
 	drawBegin( GL_POLYGON );
@@ -93,31 +81,39 @@ void RingFire::draw()
 	drawVertex2f( +7, -15 );
 	drawEnd();
 }
+RingFire::RingFire( GameObject* parent, Vector2& position, int color, double fireScale, int tick, int derivation)
+	: GameObject( parent, position ), fireScale( fireScale ), tick( tick ), derivation(derivation)
+{
+	addChild( new RingFireGraphic( this, color, 1 ) );
+	addChild( new RingFireGraphic( this, WHITE, 0.55 ) );
+}
+void RingFire::update()
+{
+	if( ++tick == 60 )
+		tick = 0;
 
+	rotation = derivation + sin( (double)tick / 30 * 3.14 ) * 15;
+
+	int t = tick<30?tick:60-tick;
+	double s = sin(t / 300.0) + 1;
+	scale = fireScale * Vector2( s );
+}
 RingLeft::RingLeft( GameObject* parent, float x )
 	: GameObject( parent, Vector2( x, 0 ) )
 {
-	const int n = 5;
+	const int n = 8;
 	const float PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899348253421170679f;
 	float radX = 50 / 2;
-	float radY = 180 / 2;
+	float radY = 200 / 2;
 	float centerX = -25 + radX;
 	float centerY = 0 + radY;
 
-    for(int k = n; k < 3*n; k++)
+    for(int k = 3*n; k > n; k--)
     {
         float circleX = centerX + radX * cos(k * PI/(2*n));
         float circleY = centerY + radY * sin(k * PI/(2*n));
-			addChild( new RingFire( this, Vector2(circleX, circleY), FIRE_RED, 0.70, rand()%60, 30));
+			addChild( new RingFire( this, Vector2(circleX, circleY), FIRE_RED, 0.70, rand()%30, 15));
 	}
-	/*
-	for(int k = 2*n; k < 3*n; k++)
-	{
-		float circleX = centerX + radX * cos(k * PI/(2*n));
-        float circleY = centerY + radY * sin(k * PI/(2*n)) - 15;
-		addChild( new RingFire( this, Vector2(circleX, circleY), FIRE, 0.45, rand()%60));
-	}
-	*/
 }
 void RingLeft::update()
 {
@@ -132,27 +128,27 @@ void RingLeft::update()
 }
 void RingLeft::draw()
 {
-	setLineWidth( 5 );
+	setLineWidth( 8 );
 	setColor( FIRE_DARK );
-	drawElipseLeft( -25, 0, 50, 180, false );
+	drawElipseLeft( -25, 0, 50, 200, false );
 	setColor( BLACK );
-	drawLine( 0, -10, 0, 5 );
+	drawLine( 0, -20, 0, 5 );
 }
 RingRight::RingRight( GameObject* parent, float x )
 	: GameObject( parent, Vector2( x, 0 ) )
 {
-	const int n = 5;
+	const int n = 8;
 	const float PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899348253421170679f;
 	float radX = 50 / 2;
-	float radY = 180 / 2;
+	float radY = 200 / 2;
 	float centerX = -25 + radX;
 	float centerY = 0 + radY;
 
-    for(int k = -n; k < n; k++)
+    for(int k = -n+1; k <= n; k++)
     {
         float circleX = centerX + radX * cos(k * PI/(2*n));
         float circleY = centerY + radY * sin(k * PI/(2*n));
-		addChild( new RingFire( this, Vector2(circleX, circleY), FIRE, 0.85, rand()%60, -30));
+		addChild( new RingFire( this, Vector2(circleX, circleY), FIRE_RED, 0.85, rand()%30, -15));
 	}
 }
 void RingRight::update()
@@ -169,7 +165,7 @@ void RingRight::update()
 }
 void RingRight::draw()
 {
-	setLineWidth( 5 );
+	setLineWidth( 8 );
 	setColor( FIRE_DARK );
-	drawElipseRight( -25, 0, 50, 180, false );
+	drawElipseRight( -25, 0, 50, 200, false );
 }
--- GAssn2/GAssn1/hurdles.h
+++ GAssn2/GAssn1/hurdles.h
@@ -20,15 +20,21 @@ public:
 	void draw();
 };
 
+class RingFireGraphic : public GameObject
+{
+public:
+	RingFireGraphic( GameObject* parent, int color, double fireScale );
+	void draw();
+private:
+	int color;
+};
 class RingFire : public GameObject
 {
 public:
 	RingFire( GameObject* parent, Vector2& position, int color, double fireScale, int tick, int derivation);
 	void update();
-	void draw();
 private:
 	int tick;
-	int color;
 	int derivation;
 	double fireScale;
 };
